0500e9abf8fb2a4d953da67cbaec9cbff56ebbf9
Teemu Koivisto <tkoivisto456@gmail.com>
2023-09-03 13:53:46 +0300

    chore: update gh actions, fix cypress test

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'cypress'
 
+const shouldSkip = false
+
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5577',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+
+      })
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--hide-scrollbars')
+          launchOptions.args.push('--high-dpi-support')
+          launchOptions.args.push('--window-size=1280,800')
+        }
+        return launchOptions
+      })
     }
   }
 })
--- packages/e2e/cypress/e2e/login.spec.cy.ts
+++ packages/e2e/cypress/e2e/login.spec.cy.ts
@@ -1,13 +1,13 @@
 describe('# Login page', () => {
   it('Should allow logging in', () => {
     cy.visit('/login').wait(1000)
-    cy.get('#login-email').type('test@asdf.fi')
+    cy.get('#login-email').type('rick@asdf.fi')
     cy.get('#login-password').type('asdfasdf')
     cy.get('button').contains('Log in').click()
     cy.wait(1)
 
-    // cy.visit('/users')
+    cy.visit('/users')
 
-    cy.get('tr').should('have.length', 3)
+    cy.get('tr').should('have.length.gte', 2)
   })
 })
