59e802e7329c14c1d4b027979571021831b0b1b0
Teemu Koivisto <tkoivisto456@gmail.com>
2023-09-03 16:26:42 +0300

    test: add wait to page load

--- packages/e2e/cypress/e2e/login.spec.cy.ts
+++ packages/e2e/cypress/e2e/login.spec.cy.ts
@@ -1,8 +1,7 @@
 describe('# Login page', () => {
   it('Should allow logging in', () => {
     cy.stubApi()
-    cy.visit('/login')
-    cy.location('pathname', { timeout: 60000 }).should('include', '/login');
+    cy.visit('/login').wait(6000)
     cy.get('#login-email').type('john@awesome.test')
     cy.get('#login-password').type('asdfasdf')
     cy.get('#login-email').should('have.value', 'john@awesome.test')
