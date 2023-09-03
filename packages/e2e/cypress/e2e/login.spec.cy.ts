9ceb76900b6972873442a4db8dcc2ccadebac673
Teemu Koivisto <tkoivisto456@gmail.com>
2023-09-03 16:06:04 +0300

    ci: even more waits

--- packages/e2e/cypress/e2e/login.spec.cy.ts
+++ packages/e2e/cypress/e2e/login.spec.cy.ts
@@ -1,12 +1,12 @@
 describe('# Login page', () => {
   it('Should allow logging in', () => {
     cy.stubApi()
-    cy.visit('/login').wait(2000)
+    cy.visit('/login').wait(4000)
     cy.get('#login-email').type('john@awesome.test')
     cy.get('#login-password').type('asdfasdf')
-    cy.get('button').contains('Log in').click().wait(500)
+    cy.get('button').contains('Log in').click().wait(2000)
 
-    cy.visit('/users').wait(500)
+    cy.visit('/users').wait(2000)
 
     cy.get('tr').should('have.length.gte', 2)
   })
