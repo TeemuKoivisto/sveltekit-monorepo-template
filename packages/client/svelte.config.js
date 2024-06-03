c96cee36f431856075f7c669d419cb7068f39c91
Teemu Koivisto <tkoivisto456@gmail.com>
2024-06-03 23:12:26 +0300

    build: add cloudflare pages deployment

--- packages/client/svelte.config.js
+++ packages/client/svelte.config.js
@@ -7,8 +7,6 @@ import autoprefixer from 'autoprefixer'
 import nested from 'postcss-nested'
 import tailwindcss from 'tailwindcss'
 
-// const { DEPLOY_TO_GH } = process.env
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -29,14 +27,10 @@ export default {
       routes: './src/routes',
       lib: './src/lib'
     },
-    // Not working as of 22.12.2022
-    // paths: {
-    //   base: DEPLOY_TO_GH ? '/sveltekit-monorepo-template' : ''
-    // },
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: '200.html'
+      fallback: '404.html'
     }),
     alias: {
       $api: 'src/api',
